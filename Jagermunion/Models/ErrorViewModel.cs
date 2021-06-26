6594d6cd39350af6af85df57888c9749fe6809f0
John-Paul Smith <jps@johnpaulsmith.co.uk>
2021-06-26 23:15:36 +0100

    Starting Website from template

---
+++ Jagermunion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jagermunion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
